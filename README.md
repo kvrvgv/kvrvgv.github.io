d052ecbf5db59f41fb17ed9c8005541f595075af
Kvrvgv <kvrvgv@yandex.ru>
2022-09-08 03:49:29 +0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# Sound Controller Bot
---
+++ app.js
@@ -0,0 +1,35 @@
+let tg = window.Telegram.WebApp;
+
+tg.expand();
+
+tg.MainButton.textColor = "#FFFFFF";
+tg.MainButton.color = "#2cab37";
+
+let item = "";
+
+let input1 = document.getElementsByTagName("input")[0]
+
+input1.addEventListener("click", function(){
+	if (tg.MainButton.isVisible) {
+		tg.MainButton.hide();
+	}
+	else {
+	    item = this.value
+		tg.MainButton.setText("Громкость: " + item);
+		tg.MainButton.show();
+	}
+});
+
+Telegram.WebApp.onEvent("mainButtonClicked", function(){
+    tg.sendData(item);
+});
+
+let usercard = document.getElementById("usercard");
+
+let p = document.createElement("p");
+
+p.innerText = `${tg.initDataUnsafe.user.first_name}
+${tg.initDataUnsafe.user.last_name}`;
+
+
+usercard.appendChild(p);
