eaa5cecfe28ccbe8bcbfba0ced413bbc4fc268de
Kvrvgv <kvrvgv@yandex.ru>
2022-09-08 05:50:25 +0300

    fix

--- app.js
+++ app.js
@@ -11,21 +11,23 @@ let input1 = document.getElementsByTagName("input")[0]
 
 input1.addEventListener("input", function(){
 	if (!(tg.MainButton.isVisible)) {
-	    item = this.value
-		if (item == 0) {
-		    input1.className = "slider muted"
-		} else {
-		if (item > 0 && item <= 33) {
-		    input1.className = "slider low"
-		} else {
-		if (item > 33 && item <= 70) {
-		    input1.className = "slider middle"
-		} else {
-		    input1.className = "slider loud"
-		}
-		}
-		}
+        tg.MainButton.show();
 	}
+	item = this.value
+    tg.MainButton.setText("Громкость: " + item + "%");
+    if (item == 0) {
+        input1.className = "slider muted"
+    } else {
+        if (item > 0 && item <= 33) {
+            input1.className = "slider low"
+        } else {
+            if (item > 33 && item <= 70) {
+                input1.className = "slider middle"
+            } else {
+                input1.className = "slider loud"
+            }
+        }
+    }
 });
 
 Telegram.WebApp.onEvent("mainButtonClicked", function(){
